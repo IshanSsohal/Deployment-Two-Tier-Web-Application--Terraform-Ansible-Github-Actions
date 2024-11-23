98b58d0e8c640d7d7cb374b136187d0c8bcbd9c5
vishnurnath10 <vishnurnath367@gmail.com>
2024-11-22 19:12:30 -0500

    Updated network module: added nat gateway-in-publicsubnet1, elastic ip and associated nat gateway to the private route table

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -75,3 +75,26 @@ resource "aws_route_table_association" "private_route_table_association" {
   route_table_id = aws_route_table.private_route_table.id
   subnet_id      = aws_subnet.private_subnet[count.index].id
 }
+
+resource "aws_eip" "nat_eip" {
+  domain = "vpc"
+
+  tags = {
+    Name = "${var.env}-nat-eip"
+  }
+}
+
+resource "aws_nat_gateway" "nat_gateway" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public_subnet[0].id
+
+  tags = {
+    Name = "${var.env}-nat-gateway"
+  }
+}
+
+resource "aws_route" "private_route_nat" {
+  route_table_id         = aws_route_table.private_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat_gateway.id
+}
