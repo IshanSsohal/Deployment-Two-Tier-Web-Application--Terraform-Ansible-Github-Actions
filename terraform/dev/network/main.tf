8bf0f63b6a3965012a8644b88648f2edcd26220b
vishnurnath10 <vishnurnath367@gmail.com>
2024-11-26 02:07:21 -0500

    added required version in dev network main.tf  issue showed from tflint

--- terraform/dev/network/main.tf
+++ terraform/dev/network/main.tf
@@ -1,7 +1,17 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
-
 module "vpc-dev" {
   source              = "../../modules/network/"
   env                 = var.env
