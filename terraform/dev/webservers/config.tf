b9611cc928bbc823156810695a8b9eee7e47a726
Ishan Sohal <ishan.s.sohal@gmail.com>
2024-11-30 04:49:25 +0000

    fixed block argument in dev/webservers/config.tf

--- terraform/dev/webservers/config.tf
+++ terraform/dev/webservers/config.tf
@@ -7,10 +7,11 @@ terraform {
       version = "~> 5.0"
     }
   }
-
-  backend "s3" {
-    bucket         = "group-7-dev"    # bucket name
-    key            = "webservers/terraform.tfstate" # Path to the state file
-    region         = "us-east-1"
-  }
 }
+
+backend "s3" {
+  bucket         = "group-7-dev"    # bucket name
+  key            = "webservers/terraform.tfstate" # Path to the state file
+  region         = "us-east-1"
+}
+  
