06872c86921dac142b4d10d07a5b73df0ed14afd
Ishan Sohal <106259626+IshanSsohal@users.noreply.github.com>
2024-11-30 00:02:16 -0500

    Update config.tf

--- terraform/dev/webservers/config.tf
+++ terraform/dev/webservers/config.tf
@@ -1,6 +1,5 @@
 #terraform {
 #   required_version = ">= 1.0.0"
-
 #   required_providers {
 #     aws = {
 #       source  = "hashicorp/aws"
@@ -10,6 +9,7 @@
 # }
 
 terraform {
+  required_version = ">= 1.0.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
