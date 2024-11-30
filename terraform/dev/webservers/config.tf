44b580996ed3f70eef46c15ac4714f6a64fe133f
Ishan Sohal <ishan.s.sohal@gmail.com>
2024-11-30 04:40:50 +0000

    fixed the syntax in dev/webservers/config.tf required block argument

--- terraform/dev/webservers/config.tf
+++ terraform/dev/webservers/config.tf
@@ -7,11 +7,13 @@ terraform {
       version = "~> 5.0"
     }
   }
+
   backend "s3" {
-    bucket         = "group-7-dev"
-    key            = "webservers/terraform.tfstate"
+    bucket         = "group-7-dev"    # bucket name
+    key            = "webservers/terraform.tfstate" # Path to the state file
     region         = "us-east-1"
   }
- }
+}
+
 
 
