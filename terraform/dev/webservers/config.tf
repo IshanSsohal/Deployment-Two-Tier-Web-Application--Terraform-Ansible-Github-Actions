f07a48bdc6d3089708e0ef3d56a7d97f0eee9171
Ishan Sohal <ishan.s.sohal@gmail.com>
2024-11-30 04:38:13 +0000

    resolved  syntax issue in dev/webservers/config.tf

--- terraform/dev/webservers/config.tf
+++ terraform/dev/webservers/config.tf
@@ -7,14 +7,11 @@ terraform {
       version = "~> 5.0"
     }
   }
-}
-
-
   backend "s3" {
     bucket         = "group-7-dev"
     key            = "webservers/terraform.tfstate"
     region         = "us-east-1"
   }
  }
-}
+
 
