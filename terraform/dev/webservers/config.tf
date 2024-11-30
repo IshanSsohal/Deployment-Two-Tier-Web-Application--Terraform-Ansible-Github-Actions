cc17a6842681a34588278a180e1f28e68b09bcf5
Ishan Sohal <ishan.s.sohal@gmail.com>
2024-11-30 04:30:00 +0000

    Resolved TFLint warnings: added required_version and variable types

--- terraform/dev/webservers/config.tf
+++ terraform/dev/webservers/config.tf
@@ -1,10 +1,14 @@
 terraform {
+  required_version = ">= 1.0.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
   }
+}
+
 
   backend "s3" {
     bucket         = "group-7-dev"
--- terraform/modules/webservers/main.tf
+++ terraform/modules/webservers/main.tf
@@ -1,4 +1,6 @@
 terraform {
+  required_version = ">= 1.0.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -6,6 +8,7 @@ terraform {
     }
   }
 }
+
 
 resource "aws_instance" "webserver" {
   count         = var.instance_count
