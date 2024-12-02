0601dbe43612328728424af9d90ce85371cc4d97
vishnurnath10 <vishnurnath367@gmail.com>
2024-12-02 15:32:54 -0500

    removed data block

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -13,10 +13,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-data "aws_availability_zones" "available" {
-  state = "available"
-}
-
 resource "aws_vpc" "main" {
   cidr_block       = var.vpc_cidr
   instance_tenancy = "default"
