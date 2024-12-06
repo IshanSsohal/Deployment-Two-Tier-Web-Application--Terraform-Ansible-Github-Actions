32f1f4057c7e753f4c18235fd79870b7cfe71659
vishnurnath10 <vishnurnath367@gmail.com>
2024-12-05 21:13:13 -0500

    removed directories

--- terraform/prod/network/config.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "group-7-prod"               
-    key    = "prod/network/terraform.tfstate"  
-    region = "us-east-1"                  
-  }
-}
--- terraform/staging/network/config.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "group-7-staging"               
-    key    = "staging/network/terraform.tfstate"  
-    region = "us-east-1"                  
-  }
-}
